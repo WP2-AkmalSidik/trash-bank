202d40fcad595150c0c105e763a77a7a54d864cf
Akmal Sidik <143138356+WP2-AkmalSidik@users.noreply.github.com>
2025-04-30 08:56:40 +0700

    feat(migration): add new migration code

---
+++ database/migrations/2025_04_30_013323_create_deposits_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('deposits', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('member_account_id')->constrained('member_accounts')->onDelete('cascade');
+            $table->foreignId('waste_type_id')->constrained('waste_types')->onDelete('cascade');
+            $table->decimal('weight_kg', 10, 2);
+            $table->decimal('total_price', 10, 2);
+            $table->timestamp('deposited_at');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('deposits');
+    }
+};
