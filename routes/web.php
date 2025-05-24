04ab594f24fa894124a2862942766473d8f4399d
Akmal Sidik <143138356+WP2-AkmalSidik@users.noreply.github.com>
2025-05-24 20:33:45 +0700

    refactor(routing): change the write code for route in larave 12

--- routes/web.php
+++ routes/web.php
@@ -16,72 +16,92 @@ use App\Http\Controllers\User\DashboardController as UserDashboardController;
 use App\Http\Controllers\Admin\DashboardController as AdminDashboardController;
 use App\Http\Controllers\Authentication\AuthController as AuthenticationController;
 
-
+// Authentication routes
 Route::controller(AuthenticationController::class)->group(function () {
     Route::get('/', 'showLoginForm')->name('login');
-    Route::post('/login', 'login')
-        ->middleware(['throttle:5,1'])
-        ->name('login.post');
+    Route::post('/login', 'login')->middleware('throttle:5,1')->name('login.post');
     Route::post('/logout', 'logout')->name('logout');
 });
 
 // Admin routes
 Route::prefix('admin')->middleware(['auth', 'role:admin'])->group(function () {
     Route::get('/dashboard', [AdminDashboardController::class, 'index'])->name('admin.dashboard');
-    // Pengumuman Routes
-    Route::get('/pengumuman', [PengumumanController::class, 'index'])->name('pengumuman.index');
-    Route::get('/pengumuman/{id}', [PengumumanController::class, 'show'])->name('pengumuman.show');
-    Route::post('/pengumuman', [PengumumanController::class, 'store'])->name('pengumuman.store');
-    Route::put('/pengumuman/{id}', [PengumumanController::class, 'update'])->name('pengumuman.update');
-    Route::delete('/pengumuman/{id}', [PengumumanController::class, 'destroy'])->name('pengumuman.destroy');
-    
-    // Lokasi Bank Sampah Routes
-    Route::get('/lokasi', [LokasiBankSampahController::class, 'index'])->name('lokasi.index');
-    Route::get('/lokasi/{id}', [LokasiBankSampahController::class, 'show'])->name('lokasi.show');
-    Route::post('/lokasi', [LokasiBankSampahController::class, 'store'])->name('lokasi.store');
-    Route::put('/lokasi/{id}', [LokasiBankSampahController::class, 'update'])->name('lokasi.update');
-    Route::delete('/lokasi/{id}', [LokasiBankSampahController::class, 'destroy'])->name('lokasi.destroy');
-    // Waste Deposit Transaction Routes
-    Route::prefix('transaksi')->group(function () {
-        Route::get('/', [TabunganController::class, 'index'])->name('transaksi.index');
-        Route::post('/store', [TabunganController::class, 'store'])->name('transaksi.store');
-        Route::delete('/histori/{id}', [TabunganController::class, 'destroy'])->name('transaksi.destroy');
-        Route::get('/get-member-data', [TabunganController::class, 'getMemberData'])->name('transaksi.get-member-data');
-        Route::get('/get-waste-price', [TabunganController::class, 'getWastePrice'])->name('transaksi.get-waste-price');
-        Route::get('/history/{memberId}', [TabunganController::class, 'history'])->name('transaksi.history');
-        Route::get('/print-receipt/{id}', [TabunganController::class, 'printReceipt'])->name('transaksi.print-receipt');
-        Route::get('/print-history/{memberId}', [TabunganController::class, 'printHistory'])->name('transaksi.print-history');
-        
-    });
-    // Nasabah
-    Route::get('/nasabah', [NasabahController::class, 'index'])->name('nasabah');
-    Route::post('/nasabah', [NasabahController::class, 'store'])->name('nasabah.store');
-    Route::post('/nasabah/generate-account', [NasabahController::class, 'generateAccountNumber'])->name('nasabah.generate-account');
-    Route::get('/nasabah/{id}/edit', [NasabahController::class, 'edit'])->name('nasabah.edit');
-    Route::put('/nasabah/{id}', [NasabahController::class, 'update'])->name('nasabah.update');
-    Route::delete('/nasabah/{id}', [NasabahController::class, 'destroy'])->name('nasabah.destroy');
-    // Waste Management Routes
-    Route::get('/sampah', [SampahController::class, 'index'])->name('sampah.index');
-    Route::post('/sampah', [SampahController::class, 'store'])->name('sampah.store');
-    Route::get('/sampah/{id}', [SampahController::class, 'show'])->name('sampah.show');
-    Route::put('/sampah/{id}', [SampahController::class, 'update'])->name('sampah.update');
-    Route::delete('/sampah/{id}', [SampahController::class, 'destroy'])->name('sampah.destroy');
-    // Residu
+
+    // Pengumuman resource routes (index, show, store, update, destroy)
+    Route::resource('pengumuman', PengumumanController::class)
+        ->names([
+            'index' => 'pengumuman.index',
+            'show' => 'pengumuman.show',
+            'store' => 'pengumuman.store',
+            'update' => 'pengumuman.update',
+            'destroy' => 'pengumuman.destroy',
+        ])->except(['create', 'edit']);
+
+    // Lokasi Bank Sampah resource routes
+    Route::resource('lokasi', LokasiBankSampahController::class)
+        ->names([
+            'index' => 'lokasi.index',
+            'show' => 'lokasi.show',
+            'store' => 'lokasi.store',
+            'update' => 'lokasi.update',
+            'destroy' => 'lokasi.destroy',
+        ])->except(['create', 'edit']);
+
+    // Residu routes (custom resource + extra print route)
     Route::prefix('residu')->group(function () {
         Route::get('/', [ResidueController::class, 'index'])->name('residu.index');
         Route::post('/', [ResidueController::class, 'store'])->name('residu.store');
+        Route::get('/print', [ResidueController::class, 'printReport'])->name('residu.print');
         Route::get('/{residu}', [ResidueController::class, 'show'])->name('residu.show');
         Route::put('/{residu}', [ResidueController::class, 'update'])->name('residu.update');
         Route::delete('/{residu}', [ResidueController::class, 'destroy'])->name('residu.destroy');
     });
-    // Pengajuan
-    Route::get('/pengajuan', [PengajuanAdmin::class, 'index'])->name('pengajuan.index');
-    Route::get('/pengajuan/filter', [PengajuanAdmin::class, 'filter'])->name('pengajuan.filter');
-    Route::post('/pengajuan/{id}/approve', [PengajuanAdmin::class, 'approve'])->name('pengajuan.approve');
-    Route::post('/pengajuan/{id}/reject', [PengajuanAdmin::class, 'reject'])->name('pengajuan.reject');
-    Route::post('/pengajuan/{id}/upload-proof', [PengajuanAdmin::class, 'uploadProof'])->name('pengajuan.upload-proof');
-    Route::get('/pengajuan/{id}/proof', [PengajuanAdmin::class, 'showProof'])->name('pengajuan.show-proof');
-    // Reports
+
+    // Transaksi routes (non-resource style with grouped prefix)
+    Route::prefix('transaksi')->controller(TabunganController::class)->group(function () {
+        Route::get('/', 'index')->name('transaksi.index');
+        Route::post('/store', 'store')->name('transaksi.store');
+        Route::delete('/histori/{id}', 'destroy')->name('transaksi.destroy');
+        Route::get('/get-member-data', 'getMemberData')->name('transaksi.get-member-data');
+        Route::get('/get-waste-price', 'getWastePrice')->name('transaksi.get-waste-price');
+        Route::get('/history/{memberId}', 'history')->name('transaksi.history');
+        Route::get('/print-receipt/{id}', 'printReceipt')->name('transaksi.print-receipt');
+        Route::get('/print-history/{memberId}', 'printHistory')->name('transaksi.print-history');
+    });
+
+    // Nasabah resource routes with extra route for generateAccountNumber
+    Route::resource('nasabah', NasabahController::class)
+        ->names([
+            'index' => 'nasabah',
+            'store' => 'nasabah.store',
+            'edit' => 'nasabah.edit',
+            'update' => 'nasabah.update',
+            'destroy' => 'nasabah.destroy',
+        ])->except(['create', 'show']);
+
+    Route::post('/nasabah/generate-account', [NasabahController::class, 'generateAccountNumber'])->name('nasabah.generate-account');
+
+    // Sampah resource routes
+    Route::resource('sampah', SampahController::class)
+        ->names([
+            'index' => 'sampah.index',
+            'store' => 'sampah.store',
+            'show' => 'sampah.show',
+            'update' => 'sampah.update',
+            'destroy' => 'sampah.destroy',
+        ])->except(['create', 'edit']);
+
+    // Pengajuan admin routes (custom, no full resource)
+    Route::prefix('pengajuan')->controller(PengajuanAdmin::class)->group(function () {
+        Route::get('/', 'index')->name('pengajuan.index');
+        Route::get('/filter', 'filter')->name('pengajuan.filter');
+        Route::post('/{id}/approve', 'approve')->name('pengajuan.approve');
+        Route::post('/{id}/reject', 'reject')->name('pengajuan.reject');
+        Route::post('/{id}/upload-proof', 'uploadProof')->name('pengajuan.upload-proof');
+        Route::get('/{id}/proof', 'showProof')->name('pengajuan.show-proof');
+    });
+
+    // Reports routes
     Route::get('/laporan', [LaporanSampahController::class, 'index'])->name('laporan.index');
     Route::get('/export-laporan', [LaporanSampahController::class, 'exportPdf'])->name('laporan.export');
 });
@@ -89,11 +109,16 @@ Route::prefix('admin')->middleware(['auth', 'role:admin'])->group(function () {
 // User routes
 Route::prefix('user')->middleware(['auth', 'role:member'])->group(function () {
     Route::get('/dashboard', [UserDashboardController::class, 'index'])->name('user.dashboard');
+
     Route::get('/transaksi', [TransaksiController::class, 'index'])->name('user.transaksi');
     Route::get('/transaksi/filter', [TransaksiController::class, 'filter'])->name('user.transaksi.filter');
+
     Route::get('/profile', [ProfileController::class, 'index'])->name('user.profile');
-    Route::get('/pengajuan', [PengajuanController::class, 'index'])->name('user.pengajuan');
-    Route::get('/pengajuan/create', [PengajuanController::class, 'create'])->name('user.pengajuan.create');
-    Route::post('/pengajuan', [PengajuanController::class, 'store'])->name('user.pengajuan.store');
-    Route::get('/pengajuan/proof/{id}', [PengajuanController::class, 'showProof'])->name('user.pengajuan.proof');
+
+    Route::prefix('pengajuan')->controller(PengajuanController::class)->group(function () {
+        Route::get('/', 'index')->name('user.pengajuan');
+        Route::get('/create', 'create')->name('user.pengajuan.create');
+        Route::post('/', 'store')->name('user.pengajuan.store');
+        Route::get('/proof/{id}', 'showProof')->name('user.pengajuan.proof');
+    });
 });
